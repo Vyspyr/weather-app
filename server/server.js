c3cae32e18efa1731df1a93a3ab0cdbf5e75b2bc
Raaed Khan <5raaed800@gmail.com>
2025-02-19 23:23:40 -0800

    finally

--- server/server.js
+++ server/server.js
@@ -2,13 +2,17 @@ import express from "express";
 import axios from "axios";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import rateLimit from "express-rate-limit";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: "https://Vyspyr.github.io/weather-app",
+    }
+));
 app.use(express.json());
 
 app.get("/weather", async (req, res) => {
@@ -30,3 +34,10 @@ app.get("/weather", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per window
+    message: "Too many requests, please try again later."
+  });
+app.use(limiter);
